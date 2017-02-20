b8682554e07358ddfa6e15bfffdb92aeffccbbe1
Estevao Beethoven Lacerda Vasques <desenvolvimento4@softmarketing.com.br>
2017-02-20 09:52:09 -0300

    Grafico de barras

--- app/js/script.js
+++ app/js/script.js
@@ -1,24 +1,23 @@
+var dataArray = [30, 15, 50];
+
+
 var canvas = d3
   .select('body')
   .append('svg')
   .attr('width', 500)
   .attr('height', 500);
 
-var circle = canvas.append('circle')
-  .attr('cx', 100)
-  .attr('cy', 150)
-  .attr('r', 50)
-  .attr('fill', 'red');
-
-var rect = canvas.append('rect')
-  .attr('width', 100)
+var bars = canvas.selectAll('rect')
+  .data(dataArray)
+  .enter()
+  .append('rect')
+  .attr('width', function(dado ){
+    return dado * 10 ;
+  })
   .attr('height', 50)
-  .attr('fill', '#999');
+  .attr('y', function(dado, index){
+    return index * 100;
+  });
 
-var line = canvas.append('line')
-  .attr('x1', 0)
-  .attr('y1', 100)
-  .attr('x2', 300)
-  .attr('y2', 200)
-  .attr('stroke', 'blue')
-  .attr('stroke-width', 5);
+console.log(bars);
+
