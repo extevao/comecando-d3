c05affd3026939a4a9df39dc929b4d9cdeef8aee
Estevao Beethoven Lacerda Vasques <desenvolvimento4@softmarketing.com.br>
2017-03-14 17:37:18 -0300

    Commit

--- app/exemplo-telecom/data.js
+++ app/exemplo-telecom/data.js
@@ -1,85 +1,85 @@
 var relacaoLigacoesOperadoras = [
     {
         nome: 'Operadora muito boa',
-        atendidas: 55,
-        naoAtendidas: 35,
-        ocupadas: 100,
         falhas: 80,
+        atendidas: 55,
+        naoatendidas: 35,
+        ocupadas: 100,
         congestionada: 25,
         blackList: 25
     }, {
         nome: 'Operadora 13',
-        atendidas: 50,
-        naoAtendidas: 35,
-        ocupadas: 100,
         falhas: 65,
+        atendidas: 50,
+        naoatendidas: 35,
+        ocupadas: 100,
         congestionada: 0,
         blackList: 0
     }, {
         nome: 'Mais uma',
-        atendidas: 250,
-        naoAtendidas: 100,
-        ocupadas: 100,
         falhas: 50,
+        atendidas: 250,
+        naoatendidas: 100,
+        ocupadas: 100,
         congestionada: 25,
         blackList: 25
     }, {
         nome: 'GVT',
-        atendidas: 70,
-        naoAtendidas: 30,
-        ocupadas: 25,
         falhas: 45,
+        atendidas: 70,
+        naoatendidas: 30,
+        ocupadas: 25,
         congestionada: 8,
         blackList: 9
     },
     {
         nome: 'Copel',
-        atendidas: 60,
-        naoAtendidas: 40,
-        ocupadas: 50,
         falhas: 40,
+        atendidas: 60,
+        naoatendidas: 40,
+        ocupadas: 50,
         congestionada: 18,
         blackList: 10
     }, {
         nome: 'OI',
-        atendidas: 30,
-        naoAtendidas: 35,
-        ocupadas: 55,
         falhas: 30,
+        atendidas: 30,
+        naoatendidas: 35,
+        ocupadas: 55,
         congestionada: 18,
         blackList: 2
     }, {
         nome: 'Teste',
-        atendidas: 5,
-        naoAtendidas: 3,
-        ocupadas: 2,
         falhas: 30,
+        atendidas: 5,
+        naoatendidas: 3,
+        ocupadas: 2,
         congestionada: 5,
         blackList: 0
     }, {
         nome: 'Sei la',
         atendidas: 45,
-        naoAtendidas: 25,
+        naoatendidas: 25,
         ocupadas: 30,
         falhas: 20,
         congestionada: 0,
         blackList: 0
     }, {
         nome: 'Sei la 2',
-        atendidas: 45,
-        naoAtendidas: 25,
-        ocupadas: 30,
         falhas: 20,
+        atendidas: 45,
+        naoatendidas: 25,
+        ocupadas: 30,
         congestionada: 0,
         blackList: 0
     }
 ];
 
 var parametrosEixoX = [
-    'atendidas',
-    'naoAtendidas',
-    'ocupadas',
     'falhas',
+    'atendidas',
+    'naoatendidas',
+    'ocupadas',
     'congestionada',
     'blackList', 
 
