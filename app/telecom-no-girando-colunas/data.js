1268f4d560a3d9bd47f09b5a6019b8b2a3924f4d
Estevao Beethoven Lacerda Vasques <desenvolvimento4@softmarketing.com.br>
2017-03-15 10:56:22 -0300

    Mais um

---
+++ app/telecom-no-girando-colunas/data.js
@@ -0,0 +1,86 @@
+var relacaoLigacoesOperadoras = [
+    {
+        nome: 'Operadora muito boa',
+        falhas: 80,
+        atendidas: 55,
+        naoatendidas: 35,
+        ocupadas: 100,
+        congestionada: 25,
+        blackList: 25
+    }, {
+        nome: 'Operadora 13',
+        falhas: 65,
+        atendidas: 50,
+        naoatendidas: 35,
+        ocupadas: 100,
+        congestionada: 0,
+        blackList: 0
+    }, {
+        nome: 'Mais uma',
+        falhas: 50,
+        atendidas: 250,
+        naoatendidas: 100,
+        ocupadas: 100,
+        congestionada: 25,
+        blackList: 25
+    }, {
+        nome: 'GVT',
+        falhas: 45,
+        atendidas: 70,
+        naoatendidas: 30,
+        ocupadas: 25,
+        congestionada: 8,
+        blackList: 9
+    },
+    {
+        nome: 'Copel',
+        falhas: 40,
+        atendidas: 60,
+        naoatendidas: 40,
+        ocupadas: 50,
+        congestionada: 18,
+        blackList: 10
+    }, {
+        nome: 'OI',
+        falhas: 30,
+        atendidas: 30,
+        naoatendidas: 35,
+        ocupadas: 55,
+        congestionada: 18,
+        blackList: 2
+    }, {
+        nome: 'Teste',
+        falhas: 30,
+        atendidas: 5,
+        naoatendidas: 3,
+        ocupadas: 2,
+        congestionada: 5,
+        blackList: 0
+    }, {
+        nome: 'Sei la',
+        atendidas: 45,
+        naoatendidas: 25,
+        ocupadas: 30,
+        falhas: 20,
+        congestionada: 0,
+        blackList: 0
+    }, {
+        nome: 'Sei la 2',
+        falhas: 20,
+        atendidas: 45,
+        naoatendidas: 25,
+        ocupadas: 30,
+        congestionada: 0,
+        blackList: 0
+    }
+];
+
+var parametrosEixoX = [
+    'falhas',
+    'atendidas',
+    'naoatendidas',
+    'ocupadas',
+    'congestionada',
+    'blackList', 
+
+    ];
---
+++ app/telecom-no-girando-colunas/telecom-no-girando-colunas.js
@@ -0,0 +1,87 @@
+var margin = { top: 20, right: 30, bottom: 30, left: 40 };
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+
+var color = d3.scale.category20();
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+
+var dataIntermediate = parametrosEixoX.map(function (parametro) {
+    return relacaoLigacoesOperadoras.map(function (relacaoOperadora) {
+        return { x: relacaoOperadora.nome, y: relacaoOperadora[parametro], legenda: parametro };
+    });
+});
+var dataStackLayout = d3.layout.stack()(dataIntermediate);
+
+x.domain(dataStackLayout[0].map(function (d) {
+    return d.x;
+}));
+
+y.domain([0,
+    d3.max(dataStackLayout[dataStackLayout.length - 1],
+        function (d) { return d.y0 + d.y; })
+]).nice();
+
+var canvas = d3.select("body")
+    .append('svg')
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+canvas.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+canvas.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+    .append('text')
+        .attr("transform", "rotate(-90)")
+        .attr('y', 6)
+        .attr('dy', '.71em')
+        .style("text-anchor", "end")
+        .text("Quantidade de ligações");
+
+var layer = canvas.selectAll('stack')
+    .data(dataStackLayout)
+    .enter()
+    .append('g');
+
+layer.selectAll("rect")
+    .data(function (d) {
+        return d;
+    })
+    .enter().append("rect")
+    .attr("x", function (d) {
+        return x(d.x);
+    })
+    .attr("y", function (d) {
+        return y(d.y + d.y0);
+    })
+    .attr("height", function (d) {
+        return y(d.y0) - y(d.y + d.y0);
+    })
+    .attr("width", x.rangeBand())
+    .attr("class", function (d, i) {
+        return d.legenda;
+    })
+    .attr("text", function(d){
+        return d.y;
+    })
