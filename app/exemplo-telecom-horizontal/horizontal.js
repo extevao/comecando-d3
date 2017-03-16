8acfd9b44fd4cdccf25a7f2c5687debf05265a86
Estevao Beethoven Lacerda Vasques <desenvolvimento4@softmarketing.com.br>
2017-03-16 15:29:12 -0300

    Adicionado legenda

--- app/exemplo-telecom-horizontal/horizontal.js
+++ app/exemplo-telecom-horizontal/horizontal.js
@@ -1,77 +1,99 @@
- var margin = { top: 20, right: 30, bottom: 30, left: 40 };
-    var width = 650 - margin.left - margin.right;
-    var height = 150 - margin.top - margin.bottom;
+var margin = { top: 20, right: 30, bottom: 30, left: 40 };
+var width = 650 - margin.left - margin.right;
+var height = 150 - margin.top - margin.bottom;
 
-    var dataIntermediate = parametrosEixoX.map(function (parametro) {
-        return relacaoLigacoesOperadoras.map(function (relacaoOperadora) {
-            return { x: relacaoOperadora.nome, y: relacaoOperadora[parametro], legenda: parametro };
-        });
+var dataIntermediate = parametrosEixoX.map(function (parametro) {
+    return relacaoLigacoesOperadoras.map(function (relacaoOperadora) {
+        return { x: relacaoOperadora.nome, y: relacaoOperadora[parametro], legenda: parametro };
+    });
+});
+
+
+var dataStackLayout = d3.layout.stack()(dataIntermediate);
+var dataset = dataStackLayout.map(function (group) {
+    return group.map(function (d) {
+        // Invert the x and y values, and y0 becomes x0
+        return {
+            x: d.y,
+            y: d.x,
+            x0: d.y0,
+            legenda: d.legenda
+        };
+    });
+});
+
+var xMax = d3.max(dataset, function (group) {
+    return d3.max(group, function (d) {
+        return d.x + d.x0;
+    });
+});
+
+var operadoras = dataset[0].map(function (d) {
+    return d.y;
+});
+
+var x = d3.scale.linear()
+    .domain([0, xMax])
+    .range([0, width])
+
+
+var y = d3.scale.ordinal()
+    .domain(operadoras)
+    .rangeRoundBands([0, height], .1);
+
+/* criando legenda do eixo x */
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom');
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left');
+/* FIM criando legenda do eixo x */
+
+
+
+var svg = d3.select('body')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+var groups = svg.selectAll('g')
+    .data(dataset)
+    .enter()
+    .append('g');
+
+var rects = groups.selectAll('rect')
+    .data(function (d) {
+        return d;
+    })
+    .enter()
+    .append('rect')
+    .attr('x', function (d) {
+        return x(d.x0);
+    })
+    .attr('y', function (d, i) {
+        return y(d.y);
+    })
+    .attr('height', function (d) {
+        return y.rangeBand();
+    })
+    .attr('width', function (d) {
+        return x(d.x);
+    }).attr("class", function (d, i) {
+        return 'bar ' + d.legenda;
     });
 
 
-    var dataStackLayout = d3.layout.stack()(dataIntermediate);
-    var dataset = dataStackLayout.map(function (group) {
-        return group.map(function (d) {
-            // Invert the x and y values, and y0 becomes x0
-            return {
-                x: d.y,
-                y: d.x,
-                x0: d.y0,
-                legenda: d.legenda
-            };
-        });
-    });
+/* Adicionando as legendas */
+svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis);
 
-    var xMax = d3.max(dataset, function (group) {
-        return d3.max(group, function (d) {
-            return d.x + d.x0;
-        });
-    });
-
-    var operadoras = dataset[0].map(function (d) {
-        return d.y;
-    });
-
-    var x = d3.scale.linear()
-        .domain([0, xMax])
-        .range([0, width])
-
-
-    var y = d3.scale.ordinal()
-        .domain(operadoras)
-        .rangeRoundBands([0, height], .1);
-
-
-
-    var svg = d3.select('body')
-        .append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-    var groups = svg.selectAll('g')
-        .data(dataset)
-        .enter()
-        .append('g');
-
-    var rects = groups.selectAll('rect')
-        .data(function (d) {
-            return d;
-        })
-        .enter()
-        .append('rect')
-        .attr('x', function (d) {
-            return x(d.x0);
-        })
-        .attr('y', function (d, i) {
-            return y(d.y);
-        })
-        .attr('height', function (d) {
-            return y.rangeBand();
-        })
-        .attr('width', function (d) {
-            return x(d.x);
-        }).attr("class", function (d, i) {
-            return 'bar ' + d.legenda;
-        })
+svg.append('g')
+    .attr('class', 'axis')
+    .call(yAxis);
+  /* FIM Adicionando as legendas */
